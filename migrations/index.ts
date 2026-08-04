5476d43ede4491f1ae4ad7e7062b6b1012957591
Majin Kami <74790927+KarmaKami994@users.noreply.github.com>
2026-08-04 20:27:14 +0200

    feat: register contact submissions migration

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260704_055801_initial from './20260704_055801_initial';
 import * as migration_20260804_190000_community_items from './20260804_190000_community_items';
+import * as migration_20260804_203000_contact_submissions from './20260804_203000_contact_submissions';
 
 export const migrations = [
   {
@@ -11,5 +12,10 @@ export const migrations = [
     up: migration_20260804_190000_community_items.up,
     down: migration_20260804_190000_community_items.down,
     name: '20260804_190000_community_items'
+  },
+  {
+    up: migration_20260804_203000_contact_submissions.up,
+    down: migration_20260804_203000_contact_submissions.down,
+    name: '20260804_203000_contact_submissions'
   },
 ];
