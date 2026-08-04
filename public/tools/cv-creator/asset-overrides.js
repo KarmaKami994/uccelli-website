50eb6a9ea93959d3384c67dee2b25fa2c689f19e
Majin Kami <74790927+KarmaKami994@users.noreply.github.com>
2026-08-04 21:46:12 +0200

    chore: use versioned CV template sources

---
+++ public/tools/cv-creator/asset-overrides.js
@@ -0,0 +1,2 @@
+ASSET_SOURCES['awesome-cv.cls'] = 'https://gist.githubusercontent.com/stifmeister1989/52bb651d12617f0c8738810d325a899a/raw/awesome-cv.cls';
+ASSET_SOURCES['res.cls'] = 'https://gist.githubusercontent.com/hawkw/9352676/raw/res.cls';
