daffc07051c256dd8fcd1b26985b4b6df43e31ca
Majin Kami <74790927+KarmaKami994@users.noreply.github.com>
2026-08-04 21:47:10 +0200

    chore: register CV creator migration

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20260704_055801_initial from './20260704_055801_initial';
 import * as migration_20260804_190000_community_items from './20260804_190000_community_items';
 import * as migration_20260804_203000_contact_submissions from './20260804_203000_contact_submissions';
+import * as migration_20260804_213000_enable_cv_creator from './20260804_213000_enable_cv_creator';
 
 export const migrations = [
   {
@@ -17,5 +18,10 @@ export const migrations = [
     up: migration_20260804_203000_contact_submissions.up,
     down: migration_20260804_203000_contact_submissions.down,
     name: '20260804_203000_contact_submissions'
+  },
+  {
+    up: migration_20260804_213000_enable_cv_creator.up,
+    down: migration_20260804_213000_enable_cv_creator.down,
+    name: '20260804_213000_enable_cv_creator'
   },
 ];
