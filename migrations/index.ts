9945ac50432042b05e9edd13e3c6ce848e49e2ef
Majin Kami <74790927+KarmaKami994@users.noreply.github.com>
2026-08-04 19:01:02 +0200

    feat: register community migration

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260704_055801_initial from './20260704_055801_initial';
+import * as migration_20260804_190000_community_items from './20260804_190000_community_items';
 
 export const migrations = [
   {
     up: migration_20260704_055801_initial.up,
     down: migration_20260704_055801_initial.down,
     name: '20260704_055801_initial'
+  },
+  {
+    up: migration_20260804_190000_community_items.up,
+    down: migration_20260804_190000_community_items.down,
+    name: '20260804_190000_community_items'
   },
 ];
